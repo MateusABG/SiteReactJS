d89a8676b744e7ffc261ca5ead0697c6374f02e0
Mateus Gautério <50017946+MateusABG@users.noreply.github.com>
2022-02-19 13:34:20 -0300

    Update firebase-config.js

--- app/src/pages/firebase-config.js
+++ app/src/pages/firebase-config.js
@@ -8,17 +8,7 @@ import {getAuth,EmailAuthProvider,GoogleAuthProvider} from 'firebase/auth';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD_58uaZQ6BEHwF9GwK0JX6dBCLgnZ225M",
-  authDomain: "app-project-interview.firebaseapp.com",
-  databaseURL: "https://app-project-interview-default-rtdb.firebaseio.com",
-  projectId: "app-project-interview",
-  storageBucket: "app-project-interview.appspot.com",
-  messagingSenderId: "947345396044",
-  appId: "1:947345396044:web:df275677cb48265ac03fe9",
-  measurementId: "G-V6D3SQBJCZ"
-};
-
+ 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db=getFirestore(app);
